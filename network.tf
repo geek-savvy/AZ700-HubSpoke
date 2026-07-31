e80fb6d2f526a5e13f3d23f2467477e66a99d950
wnash12 <wnash12@wgu.edu>
2026-07-31 16:13:39 -0400

    Added Production and Development VNets

--- network.tf
+++ network.tf
@@ -27,3 +27,37 @@ resource "azurerm_subnet" "shared" {
   virtual_network_name = azurerm_virtual_network.hub.name
   address_prefixes     = ["10.0.3.0/24"]
 }
+resource "azurerm_virtual_network" "prod" {
+  name                = "Prod-VNet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  address_space = [
+    "10.1.0.0/16"
+  ]
+}
+
+resource "azurerm_subnet" "prod_app" {
+  name                 = "AppSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.prod.name
+  address_prefixes     = ["10.1.1.0/24"]
+}
+
+resource "azurerm_virtual_network" "dev" {
+  name                = "Dev-VNet"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+
+  address_space = [
+    "10.2.0.0/16"
+  ]
+}
+
+resource "azurerm_subnet" "dev_app" {
+  name                 = "AppSubnet"
+  resource_group_name  = azurerm_resource_group.rg.name
+  virtual_network_name = azurerm_virtual_network.dev.name
+  address_prefixes     = ["10.2.1.0/24"]
+}
+
